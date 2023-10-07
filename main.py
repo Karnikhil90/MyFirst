8b69ae2d92a008032ea19d80a2caa4e6d3209771
Karnikhil90 <nikhilkarmakar4020@gmail.com>
2023-10-07 19:48:51 +0530

    some message for test

---
+++ main.py
@@ -0,0 +1,16 @@
+class new:
+    def sum(a:int,
+             b:int) -> int:
+        c = a + b
+        return c
+
+
+class get:
+    def num() -> int:
+        a = int(input("Give me a number: "))
+        return a
+
+
+answer = new.sum(get.num(),get.num())
+x = "Sum of the numbers are : "
+print(x,answer)
